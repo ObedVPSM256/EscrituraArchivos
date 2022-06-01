d2cd4de42bb23b9cfeb885e2dbd1d03d147905cc
ObedVPSM256 <105815117+ObedVPSM256@users.noreply.github.com>
2022-06-01 11:16:58 -0700

    Add files via upload
    V1

---
+++ EscrituraArchivos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            //si el archivo no existe, será creado
+            //si el archivo existe, escribirá en él
+
+            string[] lines =
+            {
+                "Esto es la primer línea",
+                "Esto es la segunda línea",
+                "Esto es la tercer línea"
+            };
+            //recorrer el arreglo para escribir las líneas en el archivo
+            foreach(string line in lines)
+            {
+                //escribir en el archivo
+                sw.WriteLine(line);
+            }
+            //cerrar archivo
+            sw.Close();
+            Console.WriteLine("Escribiendo...");
+            Console.ReadLine();
+        }
+    }
+}
