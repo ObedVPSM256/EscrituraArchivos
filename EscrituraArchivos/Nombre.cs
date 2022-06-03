5a892ee01843785701685b4597f0e161f7b28ec8
ObedVPSM256 <105815117+ObedVPSM256@users.noreply.github.com>
2022-06-02 22:20:13 -0700

    V2

---
+++ EscrituraArchivos/Nombre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscrituraArchivos
+{
+    public class Nombre
+    {
+        //atributos de clase
+        public string nombre;
+        //constructor de la clase
+        public Nombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -11,24 +11,20 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            StreamWriter sw = new StreamWriter("chikistrikis.txt", true);
             //si el archivo no existe, será creado
             //si el archivo existe, escribirá en él
 
-            string[] lines =
-            {
-                "Esto es la primer línea",
-                "Esto es la segunda línea",
-                "Esto es la tercer línea"
-            };
-            //recorrer el arreglo para escribir las líneas en el archivo
-            foreach(string line in lines)
-            {
-                //escribir en el archivo
-                sw.WriteLine(line);
-            }
+            Console.Write("Escriba un nombre: ");
+            string nombre = Console.ReadLine();
+
+            Nombre nom = new Nombre(nombre);
+
+            sw.WriteLine(nom.nombre);
+
             //cerrar archivo
             sw.Close();
+
             Console.WriteLine("Escribiendo...");
             Console.ReadLine();
         }
